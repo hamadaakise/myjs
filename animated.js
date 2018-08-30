9f493687eb9a34f4d274b7ef7903a3f65377d2b7
Aki-kun <33189731+hamadaakise@users.noreply.github.com>
2018-08-30 10:18:49 +0700

    Create animated.js

---
+++ animated.js
@@ -0,0 +1,25 @@
+<script src="https://animated.js"></script>
+
+<div class="animated_bg">
+
+</div>
+
+
+<script>
+	JQuery(document).ready(function(){
+
+	$('.animated_bg2').animatedBG({
+	colorSet: ['#007acc','#008ae6','#0099ff','#1aa3ff'],
+	speed: 2000
+	});
+
+	$('.animated_bg3').animatedBG({
+	colorSet: ['#00b35f','#00cc6d','#00e67a','#009933'],
+	speed: 6000
+	});
+
+});
+
+</script>
+
+
